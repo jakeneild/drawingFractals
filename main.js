4f42d3fac865c216b083f1ddd7869f477f1826a4
Karl Heitmann <khf1988@gmail.com>
2021-01-31 20:50:56 -0300

    for correctly indented. Erased first call to createCanvas and put it inside a main function

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-createCanvas(2500, .7, .6, 100);
-
 var delayInMilliseconds = 5000; //1 second
 
 var arg1 = 2500;
@@ -8,15 +6,28 @@ var arg3 = .6;
 var arg4 = 100;
 var counter = 0;
 
-for(i = 0; i < 2; i++){  //can't get this working smoothly.  Maybe my computer just isn't powerful enough?
-    arg1 = arg1 + 100;
-    //arg2 = arg2 - .5;
-    //arg3 = arg3 + .5;
-    setTimeout(function() {
-        document.body.innerHTML = "";
-        createCanvas(arg1, arg2, arg3, arg4);
-      }, delayInMilliseconds);
+// for(i = 0; i < 2; i++){  //can't get this working smoothly.  Maybe my computer just isn't powerful enough?
+//     arg1 = arg1 + 100;
+//     //arg2 = arg2 - .5;
+//     //arg3 = arg3 + .5;
+//     setTimeout(function() {
+//         document.body.innerHTML = "";
+//         createCanvas(arg1, arg2, arg3, arg4);
+//     }, delayInMilliseconds);
+// }
+
+// createCanvas(2500, .7, .6, 100);
+// createCanvas(arg1, arg2, arg3, arg4);
+
+
+async function main() {
+    createCanvas(arg1, arg2, arg3, arg4)
+    await setTimeout(() => {
+        console.log("timeout")
+    }, 1000)
 }
+
+main()
 
 
 
@@ -48,26 +59,27 @@ function createCanvas(a, b, c, d){
             imaginaryComponentOfResult = tempImaginaryComponent;
 
             // Return a number as a percentage
-            if(realComponentOfResult * imaginaryComponentOfResult > 5) 
+            if(realComponentOfResult * imaginaryComponentOfResult > 5)
                 return (i/maxIterations * 100);
         }
-        return 0;   // Return zero if in set        
-    }  
+        return 0;   // Return zero if in set
+    }
 
     for(var x=0; x < myCanvas.width; x++) {
-    for(var y=0; y < myCanvas.height; y++) {
-        var belongsToSet = 
-                checkIfBelongsToMandelbrotSet(x/magnificationFactor - panX, 
-                                            y/magnificationFactor - panY);
-        if(belongsToSet == 0) {
-            ctx.fillStyle = '#000';
-            ctx.fillRect(x,y, 1,1); // Draw a black pixel
-        } else {
-            ctx.fillStyle = 'hsl(0, 100%, ' + belongsToSet + '%)';
-            ctx.fillRect(x,y, 1,1); // Draw a colorful pixel
-        }               
-    } 
+        for(var y=0; y < myCanvas.height; y++) {
+            var belongsToSet =
+                    checkIfBelongsToMandelbrotSet(x/magnificationFactor - panX,
+                                                y/magnificationFactor - panY);
+            if(belongsToSet == 0) {
+                ctx.fillStyle = '#000';
+                ctx.fillRect(x,y, 1,1); // Draw a black pixel
+            } else {
+                ctx.fillStyle = 'hsl(0, 100%, ' + belongsToSet + '%)';
+                ctx.fillRect(x,y, 1,1); // Draw a colorful pixel
+            }
+        }
     }
+    console.log("FINISH")
     counter++;
 }
 
